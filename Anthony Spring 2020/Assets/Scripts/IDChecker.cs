19b5e81f74366f88d34b3255687d755789814319
School Computer <stevOOOOOOOOOO0@yahoo.com>
2020-01-14 13:43:09 -0700

    Code for an ID checker

---
+++ Anthony Spring 2020/Assets/Scripts/IDChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchID : MonoBehaviour
+{
+    public NameID nameIDObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var nameIdObj = other.GetComponent<MatchID>().nameIDObj;
+        if (nameIdObj == null) return;
+        var otherNameId = nameIdObj;
+        if (nameIDObj == otherNameId)
+        {
+            //do work
+        }
+    }
+}
