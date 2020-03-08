89e6df971e3fd3e8ac0c609511b9e90524c8e08a
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2020-03-08 01:58:09 -0600

    updates the text to display the Dice number it lands on

---
+++ Anthony Spring 2020/Assets/Dice/ChangeUIText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeUIText : MonoBehaviour
+{
+    public Text text;
+    public StringData textData;
+
+    public void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void SetTextToStringData()
+    {
+        text.text = textData.Value;
+    }
+}
